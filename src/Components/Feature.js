64969fd5eaf600d3787c87ee346d77eb40e96123
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-11-22 19:36:00 +0500

    Update Feature.js

--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -50,7 +50,7 @@ function Feature() {
                         </div>
                     </div>
                     <center>
-                        <div className="pt-40 w-auto">
+                        <div className="pt-40 w-auto mx-24">
                             <video className=" rounded-3xl" controls>
                                 <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" type="video/mp4" />
                             </video>
