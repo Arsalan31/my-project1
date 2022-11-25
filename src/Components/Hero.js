5ea9ee6089847dbc45c6f4629f234b9a1a9ff41a
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-11-25 15:27:43 +0500

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -48,12 +48,12 @@ function Hero() {
     const toggle2 = () => {
         var x = document.getElementById("feature")
 
-        if(x.style.display === "none"){
-            x.style.display = "block";
+        if(x.style.display === "block"){
+            x.style.display = "none";
         }
 
         else{
-            x.style.display = "none";
+            x.style.display = "block";
         }
     }
     return (
