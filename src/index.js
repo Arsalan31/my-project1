544a76309e395c0131e9782256594dfcd8b22e7d
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-01 16:37:36 +0500

    new

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Pages/App';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Udix from './Udix';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+    <Routes>
+      <Route path='home' element={<Udix />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
