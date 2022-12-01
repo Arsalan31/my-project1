4541b8c00312f1388a6469441fc762d5d898cb15
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-01 16:24:20 +0500

    page

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Footer from "./Components/Footer";
-import Pricing from "./Components/Pricing";
-import Contact from "./Components/Contact";
-import Hero from "./Components/Hero";
-import Feature from "./Components/Feature";
-import Partners from "./Components/Partners";
-import Header from "./Components/Header";
+import Footer from "../Components/Footer";
+import Pricing from "../Components/Pricing";
+import Contact from "../Components/Contact";
+import Hero from "../Components/Hero";
+import Feature from "../Components/Feature";
+import Partners from "../Components/Partners";
+import Header from "../Components/Header";
 
 
 
