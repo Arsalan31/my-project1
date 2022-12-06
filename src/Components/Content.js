bbe8e1bbec7a9e4d8fc7a4c099e2c7bb630e6f1e
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-06 13:40:34 +0500

    new

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -8,7 +8,7 @@ function Content() {
             <div className="container px-16 pb-6 mx-auto">
                 <div className="flex flex-wrap-reverse lg:flex-wrap justify-center">
                     <div className="flex flex-col flex-none flex-grow-0 order-1 text-center lg:w-1/2 mt-32 mb-20">
-                    
+                        <div className="image1 mt-36"></div>
                     </div>
                     <div className="flex flex-col flex-none flex-grow-0 order-0 md:order-1 text-center w-full lg:w-1/2 mt-32 mb-20">
                         <img className="image md:self-end self-center mt-24"></img>
@@ -18,6 +18,15 @@ function Content() {
                             <br className="sm:block" />mobile and video ads and many more. The campaigns either support our
                             <br className="sm:block" /> publishing assets or (in most cases) direct users directly to our clients.</p>
                     </div>
+                </div>
+                <div className="flex -mt-36 justify-end text-gray-300">
+                    <button><svg xmlns="http://www.w3.org/2000/svg" className="me-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                    </svg></button>
+                    <h4>1 / 2</h4>
+                    <button><svg xmlns="http://www.w3.org/2000/svg" className="ms-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                    </svg></button>
                 </div>
             </div>
 
