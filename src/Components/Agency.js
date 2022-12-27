d121c87f8a8fb49c5a826634d17cc4b90c5aa864
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-27 20:56:25 +0500

    changes

--- src/Components/Agency.js
+++ src/Components/Agency.js
@@ -21,65 +21,62 @@ function Agency() {
                             <div class="flex-grow sm:px-6 py-5 sm:block inline-flex">
                                 <div className="aka sm:-mt-0 -mt-8 sm:w-18 sm:h-18 w-18 h-18">
                                 </div>
-                                <p className="text-xs sm:hidden flex font-bold">Aga Khan Academies</p>
+                                <p className="text-xs sm:hidden sm:ms-0 ms-5 flex font-bold">Aga Khan Academies</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 sm:flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 md:mb-auto sm:mb-7">
                             <div class="sm:flex-grow sm:w-auto w-full flex-none sm:block inline-flex sm:px-6 py-3">
-                                <div className="akh sm:h-12 sm:w-42 w-28 h-12"></div>
-                                <p class="text-xs font-bold mt-3 sm:w-full">Aga Khan Agency For Habitat</p>
+                                <div className="akh sm:-ms-0 -ms-14 sm:h-12 sm:w-42 w-28 h-12"></div>
+                                <p class="text-xs sm:ms-0 ms-3 font-bold mt-3 sm:w-full">Aga Khan Agency For Habitat</p>
                             </div>
 
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-4 sm:block inline-flex">
-                                <div className="akm sm:flex hidden sm:h-19 sm:w-48"></div>
-                                <div className="akm1 sm:hidden flex sm:h-19 sm:w-48 w-10 h-9"></div>
-                                <p class="text-xs sm:hidden flex font-bold mt-2 w-full">Aga Khan Agency For Microfinance</p>
+                                <div className="akm flex xl:ms-16 lg:ms-7 sm:ms-12 sm:h-9 sm:w-12 w-12 h-9"></div>
+                                <p class="text-xs sm:ms-0 ms-6 flex font-bold mt-2 w-full">Aga Khan Agency For Microfinance</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-5 sm:block inline-flex">
-                                <div className="aks sm:h-13 sm:w-36 w-24 h-12"></div>
-                                <p class="text-xs font-bold sm:mt-1 mt-3">Aga Khan Education Services</p>
+                                <div className="aks sm:-ms-0 -ms-12 sm:h-13 sm:w-36 w-24 h-12"></div>
+                                <p class="text-xs sm:ms-0 ms-4 font-bold sm:mt-1 mt-3">Aga Khan Education Services</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-3 sm:block inline-flex">
-                                <div className="akf sm:h-15 sm:w-36 w-32 h-15"></div>
-                                <p class="text-xs sm:mt-1 mt-6 font-bold">Aga Khan Foundation</p>
+                                <div className="akf sm:-ms-0 -ms-12 sm:h-15 sm:w-36 w-32 h-15"></div>
+                                <p class="text-xs sm:-ms-0 -ms-4 sm:mt-1 mt-6 font-bold">Aga Khan Foundation</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow py-6 sm:block inline-flex">
-                                <div className="aked h-10 w-36 xl:ms-14 sm:ms-11"></div>
-                                <p class="text-xs font-bold mt-3">Aga Khan Fund For Economic Development</p>
+                                <div className="aked -ms-10 h-10 w-36 xl:ms-12 lg:ms-7 sm:ms-11"></div>
+                                <p class="text-xs sm:-ms-0 -ms-10 font-bold mt-3">Aga Khan Fund For Economic Development</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-3 sm:block inline-flex">
-                                <div className="akhs sm:flex hidden sm:h-22 sm:w-42"></div>
-                                <div className="akhs1 sm:hidden flex h-10 w-11"></div>
-                                <p class="text-xs sm:hidden flex font-bold mt-3">Aga Khan Health Services</p>
+                                <div className="akhs flex xl:ms-12 lg:ms-7 sm:ms-12 h-10 w-11"></div>
+                                <p class="text-xs sm:ms-0 ms-5 flex font-bold mt-3">Aga Khan Health Services</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-5 sm:block inline-flex">
-                                <div className="aktc sm:flex hidden sm:h-18 sm:w-42"></div>
-                                <div className="aktc1 sm:hidden flex h-7 w-11"></div>
-                                <p class="text-xs sm:hidden flex font-bold mt-3">Aga Khan Trust for Culture</p>
+                                <div className="aktc flex xl:ms-12 lg:ms-7 sm:ms-12 h-7 w-11"></div>
+                                <p class="text-xs sm:ms-0 ms-5 flex font-bold mt-3">Aga Khan Trust for Culture</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-3 sm:block inline-flex">
-                                <div className="aku sm:ms-8 h-16 w-20 sm:h-16 sm:w-20"></div>
+                                <div className="aku -ms-4 sm:ms-8 h-16 w-20 sm:h-16 sm:w-20"></div>
                                 <p class="text-xs font-bold uppercase sm:mt-0 mt-7">The Aga Khan University</p>
                             </div>
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-3 sm:block inline-flex">
-                                <div className="uca w-14 h-14 sm:w-14 sm:h-14 xl:ms-12 lg:ms-6 md:ms-10 sm:ms-14"></div>
-                                <p class="text-xs sm:mt-3 font-bold uppercase mt-4">University Of Central Asia</p>
+                                <div className="uca -ms-2 w-14 h-14 sm:w-14 sm:h-14 xl:ms-12 lg:ms-6 md:ms-10 sm:ms-14"></div>
+                                <p class="text-xs sm:ms-0 ms-4 sm:mt-3 font-bold uppercase mt-4">University Of Central Asia</p>
                             </div>
                         </div>
                     </div>
