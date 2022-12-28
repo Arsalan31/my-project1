39e1e1c240c14241cec90df3ecd16309848bd788
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-28 18:51:26 +0500

    new

--- src/Components/Agency.js
+++ src/Components/Agency.js
@@ -10,11 +10,13 @@ function Agency() {
                     <div className="w-2/3">
                         <p className="font-bold sm:text-2xl text-xl uppercase">our agency</p>
                     </div>
-                    <div className="sm:flex hidden w-1/3 justify-end">
-                        <p className="font-medium text-base text-blue-70">How We Work </p>
-                    </div>
-                    <div className="flex sm:hidden w-1/3 justify-end">
-                        <p className="font-medium text-base text-blue-70">See More </p>
+                    <div className="flex w-1/3 justify-end">
+                        <a href="#" className="flex gap-4 focus:outline-none">
+                            <p className="font-medium text-base text-blue-70">How We Work </p>
+                            <svg className="mt-1" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 13L7 7L1 1" stroke="#084C61" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </a>
                     </div>
                     <div class="flex flex-wrap sm:justify-center justify-start mt-10 ">
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 sm:flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 md:mb-auto sm:mb-7">
@@ -29,11 +31,10 @@ function Agency() {
                                 <div className="akh sm:-ms-0 -ms-14 sm:h-12 sm:w-42 w-28 h-12"></div>
                                 <p class="text-xs sm:ms-0 ms-3 font-bold mt-3 sm:w-full">Aga Khan Agency For Habitat</p>
                             </div>
-
                         </div>
                         <div class="sm:p-4 lg:w-1/5 md:w-1/3 w-full sm:w-60 sm:h-36 flex h-20 flex-col text-center items-start sm:items-center sm:border border-b border-gray-300 sm:mb-7">
                             <div class="flex-grow sm:px-6 py-4 sm:block inline-flex">
-                                <div className="akm flex xl:ms-16 lg:ms-7 sm:ms-12 sm:h-9 sm:w-12 w-12 h-9"></div>
+                                <div className="akm flex xl:ms-16 lg:ms-7 sm:ms-12 sm:h-9 sm:w-12 w-12 h-9 bg-no-repeat"></div>
                                 <p class="text-xs sm:ms-0 ms-6 flex font-bold mt-2 w-full">Aga Khan Agency For Microfinance</p>
                             </div>
                         </div>
