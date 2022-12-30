826b5466cb693e6b718738ca728304380f233fff
Arsalan31 <arsalanzulfiqar31@gmail.com>
2022-12-30 13:03:54 +0500

    new

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -41,7 +41,7 @@ function Content(props) {
                             <SwiperSlide>
                                 <div className="flex flex-col flex-none flex-grow-0 text-center w-full mb-20">
                                     <div class="flex flex-wrap lg:-m-4 -mx-4 -mb-10 lg:space-y-0 space-y-6">
-                                        <div class="p-4 lg:w-1/2 lg:px-14 w-full order-1 lg:order-0 flex flex-col text-center">
+                                        <div class="p-4 lg:w-1/2 lg:px-14 w-full order-1 lg:order-0 flex flex-col lg:items-start items-center">
                                             <div className="image1 lg:w-100 lg:h-92 sm:w-120 sm:h-98 w-80 h-60 lg:mt-36"></div>
                                         </div>
                                         <div class="p-4 lg:w-1/2 lg:px-22 w-full order-0 lg:order-1 flex flex-col text-center">
@@ -58,7 +58,7 @@ function Content(props) {
                             <SwiperSlide>
                                 <div className="flex flex-col flex-none flex-grow-0 order-0 lg:order-1 text-center w-full mb-20">
                                     <div class="flex flex-wrap lg:-m-4 -mx-4 -mb-10 lg:space-y-0 space-y-6">
-                                        <div class="p-4 lg:w-1/2 lg:px-14 w-full order-1 lg:order-0 flex flex-col text-center">
+                                        <div class="p-4 lg:w-1/2 lg:px-14 w-full order-1 lg:order-0 flex flex-col lg:items-start items-center">
                                             <div className="image1 lg:w-100 lg:h-92 sm:w-120 sm:h-98 w-80 h-60 lg:mt-36"></div>
                                         </div>
                                         <div class="p-4 lg:w-1/2 lg:px-22 w-full order-0 lg:order-1 flex flex-col text-center">
