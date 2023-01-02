e0247cba2b006bc0004b9f616cffd83c8bd92d8d
Arsalan31 <arsalanzulfiqar31@gmail.com>
2023-01-02 15:57:16 +0500

    new

--- src/Components/Hero2.js
+++ src/Components/Hero2.js
@@ -5,20 +5,20 @@ import './style.css';
 function Hero2() {
     return (
         <section>
-            <div className="container mt-36 sm:p-14 px-5 py-6 mx-auto bg-white">
-                <div className="bg rounded-ts-3xl px-16 pt-32 rounded-te-3xl rounded-bs-3xl rounded-be-7xl">
+            <div className="lg:container lg:mt-36 lg:p-14 lg:px-5 lg:py-6 mx-auto bg-white">
+                <div className="bg lg:rounded-ts-3xl px-16 pt-32 lg:rounded-te-3xl lg:rounded-bs-3xl lg:rounded-be-7xl">
                     <h3 className="text-6xl lg:w-2/3 w-full lg:text-start text-center text-white font-semibold">Practice your Photography skills with ease</h3>
                     <div className="flex lg:flex-nowrap flex-wrap">
                         <div className="lg:w-3/4 w-full">
                             <h4 className="text-white lg:w-7/12 w-full lg:text-start text-center text-semibold text-xl lg:mt-0 mt-10">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
                             </h4>
-                            <div className="flex lg:justify-start justify-center gap-4 mt-8 pb-10">
+                            <div className="flex flex-wrap lg:justify-start justify-center gap-4 mt-8 pb-10">
                                 <button className="text-white px-8 py-2 text-xl font-bold bg-pink-10 rounded-xl focus:outline-none">Get Started</button>
                                 <button className="text-white border border-white px-8 py-2 rounded-xl text-xl font-bold focus:outline-none">Discount</button>
                             </div>
                         </div>
-                        <div className="lg:w-10/12 w-full lg:-mt-52 mt-16">
+                        <div className="flex justify-center lg:w-10/12 w-full lg:-mt-52 mt-16">
                             <img src={require('./assets/Saly-10.png')}></img>
                         </div>
                     </div>
--- src/Components/Subscribe.js
+++ src/Components/Subscribe.js
@@ -8,10 +8,10 @@ function Subscribe() {
                     <h3 className="text-center text-3xl text-purple-650 font-semibold">Subscribe to get information, latest news and other
                         <br />
                         interesting offers about WindTravel</h3>
-                    <div className="flex justify-center gap-6 mt-20">
+                    <div className="flex flex-wrap justify-center gap-6 mt-20">
                         <label for="email" className="leading-7 text-sm text-black"></label>
-                        <input type="email" id="email" name="email" placeholder="Your Email" className="w-1/4 h-14 placeholder-black bg-white border rounded-2xl border-gray-200 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm font-normal outline-none text-blue-1000 p-8 leading-8 transition-colors duration-200 ease-in-out" />
-                        <button className="bg-peach-10 px-16 rounded-xl">
+                        <input type="email" id="email" name="email" placeholder="Your Email" className="w-1/3 h-14 placeholder-black bg-white border rounded-2xl border-gray-200 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm font-normal outline-none text-blue-1000 p-8 leading-8 transition-colors duration-200 ease-in-out" />
+                        <button className="bg-peach-10 px-16 rounded-xl text-white">
                                 Subscribe
                         </button>
                     </div>
