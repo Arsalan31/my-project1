b086a1aef73e63ec2209f0bc9a51f22fd64caecb
Arsalan31 <arsalanzulfiqar31@gmail.com>
2023-01-02 18:18:03 +0500

    new

--- src/Components/Customers.js
+++ src/Components/Customers.js
@@ -115,11 +115,11 @@ function Customers(props) {
                     </>
                 </div>
                 <div className="flex  lg:order-2 order-1 justify-center text-gray-400 md:text-gray-400">
-                    <button className="swiper-button-prev1 transform rotate-180 focus:outline-none"><svg xmlns="http://www.w3.org/2000/svg" className="me-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <button className="swiper-button-prev1 focus:outline-none"><svg xmlns="http://www.w3.org/2000/svg" className="me-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                     </svg></button>
                     <div class="swiper-pagination"></div>
-                    <button className="swiper-button-next1 transform rotate-180 focus:outline-none"><svg xmlns="http://www.w3.org/2000/svg" className="ms-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <button className="swiper-button-next1 focus:outline-none"><svg xmlns="http://www.w3.org/2000/svg" className="ms-3" width="60" height="30" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                     </svg></button>
                 </div>
