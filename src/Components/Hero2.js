3669a55ddb7b641b5e48f13e4f5da82bfee900c1
Arsalan31 <arsalanzulfiqar31@gmail.com>
2023-01-02 13:26:22 +0500

    Update Hero2.js

--- src/Components/Hero2.js
+++ src/Components/Hero2.js
@@ -6,25 +6,30 @@ function Hero2() {
     return (
         <section>
             <div className="container mt-36 sm:p-14 px-5 py-6 mx-auto bg-white">
-                <div className="bg rounded-ts-3xl p-20 rounded-te-3xl rounded-bs-3xl rounded-be-7xl">
-                    <div className="">
-                        <h3 className="text-start text-6xl w-8/12 text-white font-semibold">Practice your Photography skills with ease</h3>
-                        <h4 className="text-white w-1/3 text-semibold text-xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
-                        </h4>
-                        <div className="flex gap-4 mt-8">
-                            <button className="text-white px-8 py-2 text-xl font-bold bg-pink-10 rounded-xl">Get Started</button>
-                            <button className="text-white border border-white px-8 py-2 rounded-xl text-xl font-bold">Discount</button>
+                <div className="bg rounded-ts-3xl px-16 pt-32 rounded-te-3xl rounded-bs-3xl rounded-be-7xl">
+                    <h3 className="text-6xl lg:w-2/3 w-full lg:text-start text-center text-white font-semibold">Practice your Photography skills with ease</h3>
+                    <div className="flex lg:flex-nowrap flex-wrap">
+                        <div className="lg:w-3/4 w-full">
+                            <h4 className="text-white lg:w-7/12 w-full lg:text-start text-center text-semibold text-xl lg:mt-0 mt-10">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+                            </h4>
+                            <div className="flex lg:justify-start justify-center gap-4 mt-8 pb-10">
+                                <button className="text-white px-8 py-2 text-xl font-bold bg-pink-10 rounded-xl focus:outline-none">Get Started</button>
+                                <button className="text-white border border-white px-8 py-2 rounded-xl text-xl font-bold focus:outline-none">Discount</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="w-1/2 flex content-end">
-                        <div className="flex justify-center gap-6 mt-20">
+                        <div className="lg:w-10/12 w-full lg:-mt-52 mt-16">
                             <img src={require('./assets/Saly-10.png')}></img>
                         </div>
                     </div>
+                    {/* <div className="w-1/2 flex content-end">
+                        <div className="flex justify-center gap-6 mt-20">
+                            <img src={require('./assets/Saly-10.png')}></img>
+                        </div>
+                    </div> */}
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
