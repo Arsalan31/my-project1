5b7240e2695e7872c234fafaa9e0b1cdeb60c9de
Arsalan31 <arsalanzulfiqar31@gmail.com>
2023-01-02 18:38:50 +0500

    Create Partners2.js

---
+++ src/Components/Partners2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+
+function Partners2(){
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Partners2;
