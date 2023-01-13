6f020be79e65141d9e84a626dd77bfc47cfd91bf
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-01-13 20:15:04 +0500

    Update Partners2.js

--- src/Components/Partners2.js
+++ src/Components/Partners2.js
@@ -68,7 +68,7 @@ function Partners2(props) {
         <section className="sm:mt-10 lg:mt-0 flex flex-col items-start w-full h-full bg-transparent flex-none order-2 flex-grow-0">
             <div className="container mt-8 sm:px-16 px-8 pb-6 mx-auto">
                 <h4 className="text-red-500 text-3xl font-bold text-center">Our Partnership Projects</h4>
-                <div className="container mx-auto lg:border-gray-400 lg:border-b border-red-500 border-4 lg:w-1/5 w-1/4 flex justify-center mt-5"></div>
+                <div className="container mx-auto border-b lg:border border-red-500 border-4 lg:w-1/5 w-1/4 flex justify-center mt-5"></div>
                 <div className="flex flex-wrap justify-center mt-10">
                     <>
                         <Swiper
