2b1404b13a52a3f182bf2e756aa0c3ca022d18fd
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-01-25 19:22:14 +0500

    Update Subscribe.js

--- src/Components/Subscribe.js
+++ src/Components/Subscribe.js
@@ -9,23 +9,21 @@ function Subscribe() {
                 </div>
                 <div className="flex justify-end">
                     <img className="absolute opacity-10" src={require('./assets/Group 42 (1).png')}></img>
-                </div>  
+                </div>
                 <div className="bg-purple-50 sm:py-20 sm:px-10 p-8 md:rounded-ts-7xl rounded-ts-4xl rounded-ts-3xl rounded-te-3xl rounded-bs-3xl rounded-be-3xl">
-                    {/* <div className="flex justify-end">
-                        <img className="absolute"src={require('./assets/Group 42 (1).png')}></img>
-                    </div> */}
                     <h3 className="text-center text-3xl text-purple-650 font-semibold">Subscribe to get information, latest news and other
                         <br />
-                        interesting offers about WindTravel</h3>
+                        interesting offers about WindTravel
+                    </h3>
+                    <div className="flex justify-start">
+                        <img className="absolute opacity-10 transform rotate-90" src={require('./assets/Group 42 (1).png')}></img>
+                    </div>
                     <div className="flex md:flex-nowrap flex-wrap justify-center gap-6 mt-20">
                         <input type="email" id="email" name="email" placeholder="Your Email" className="text1 text2 lg:w-1/3 md:w-2/3 w-full h-14 bg-white border rounded-2xl border-gray-200 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-sm font-normal outline-none placeholder-blue-1000 text-blue-1000 p-8 px-14 leading-8 transition-colors duration-200 ease-in-out" />
                         <button className="bg-peach-10 lg:w-1/5 md:w-1/3 w-full px-10 h-16 rounded-xl text-white">
                             Subscribe
                         </button>
                     </div>
-                    {/* <div className="flex justify-start">
-                        <img className="opacity-10 absolute" src={require('./assets/Group 42.png')}></img>
-                    </div> */}
                 </div>
                 <div className="sm:flex hidden justify-end absolute lg:end-24 sm:end-20">
                     <img src={require('./assets/Group 5.png')} className="sm:flex hidden justify-end relative bottom-24 sm:-end-20"></img>
