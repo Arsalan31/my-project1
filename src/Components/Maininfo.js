2f36ecb8138e97855ace5e37fc8a300d9e0151cc
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-01-31 14:21:54 +0500

    new

--- src/Components/Maininfo.js
+++ src/Components/Maininfo.js
@@ -2,8 +2,8 @@ import React, {useState}  from "react";
 
 function Maininfo(){
     return(
-        <section>
-            
+        <section className="px-24 pt-12 container mx-auto w-full h-full">
+            <h4 className="text-2xl font-bold text-gray-5 md:text-center">AKDN | Education</h4>
         </section>
     )
 }
