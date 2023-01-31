84fa1774818be0a54dea7f4a8dc9a62488cea7f7
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-01-31 14:07:02 +0500

    Update Info.js

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,6 +1,27 @@
 import React, { useState } from "react"
 
 function Info() {
+
+    const ReadMore = ({ children }) => {
+        const text = children;
+        const [isReadMore, setIsReadMore] = useState(true)
+        const toggleReadMore = () => {
+            setIsReadMore(!isReadMore)
+        }
+        return (
+            <p className="text">
+                {isReadMore ? text.slice(0, 158) : text}
+                <div className="flex md:mt-3 mt-2 justify-start">
+                    <button className="flex gap-4 font-medium text-base text-blue-70 justify-start focus:outline-none" onClick={toggleReadMore}>See More
+                        <svg className="transform rotate-90 mt-1.5" width="6" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 13L7 7L1 1" stroke="#084C61" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+            </p>
+        );
+    };
+
     return (
         <section className="p-12 container mx-auto w-full h-full">
             <h4 className="font-bold text-2xl uppercase text-start">Our work</h4>
@@ -8,8 +29,49 @@ function Info() {
                 <div className="md:w-2/3 w-full">
                     <p className="font-normal text-base text-gray-5 uppercase text-start">Early Childhood Development</p>
                 </div>
-                <div className="md:w-1/2 w-full">
-                    <p className="text-gray-5">Globally, the AKDN's activities in early childhood development provide 750,000 children aged pre-natal-8 with quality early learning opportunities, annually.</p>
+                <div className="md:w-3/4 w-full">
+                    <p className="text-gray-5 font-normal text-xl">
+                        <ReadMore className="text-gray-5 font-normal text-xl">
+                            Globally, the AKDN's activities in early childhood development provide 750,000 children aged pre-natal-8 with quality early learning opportunities, annually. The project is a joint initiative led by the Aga Khan Health Services Tanzania (AKHST) together with the Aga Khan Foundation Tanzania (AKFT),It aims to strengthen and expand the quality, access, and capacity of cancer care services across the cancer continuum in Dar es Salaam and Mwanza regions of Tanzania.For over 100 years the Aga Khan Development Network (AKDN) has worked to ensure that students of all ages have access to quality learning opportunities.
+                        </ReadMore>
+                    </p>
+                </div>
+            </div>
+            <div className="md:flex mt-6">
+                <div className="md:w-2/3 w-full">
+                    <p className="font-normal text-base text-gray-5 uppercase text-start">Primary & Secondary
+                    </p>
+                </div>
+                <div className="md:w-3/4 w-full">
+                    <p className="text-gray-5 font-normal text-xl">
+                        <ReadMore className="text-gray-5 font-normal text-xl">
+                            Globally, the AKDN's activities in early childhood development provide 750,000 children aged pre-natal-8 with quality early learning opportunities, annually. The project is a joint initiative led by the Aga Khan Health Services Tanzania (AKHST) together with the Aga Khan Foundation Tanzania (AKFT),It aims to strengthen and expand the quality, access, and capacity of cancer care services across the cancer continuum in Dar es Salaam and Mwanza regions of Tanzania.For over 100 years the Aga Khan Development Network (AKDN) has worked to ensure that students of all ages have access to quality learning opportunities.
+                        </ReadMore>
+                    </p>
+                </div>
+            </div>
+            <div className="md:flex mt-6">
+                <div className="md:w-2/3 w-full">
+                    <p className="font-normal text-base text-gray-5 uppercase text-start">Higher Education</p>
+                </div>
+                <div className="md:w-3/4 w-full">
+                    <p className="text-gray-5 font-normal text-xl">
+                        <ReadMore className="text-gray-5 font-normal text-xl">
+                            Globally, the AKDN's activities in early childhood development provide 750,000 children aged pre-natal-8 with quality early learning opportunities, annually. The project is a joint initiative led by the Aga Khan Health Services Tanzania (AKHST) together with the Aga Khan Foundation Tanzania (AKFT),It aims to strengthen and expand the quality, access, and capacity of cancer care services across the cancer continuum in Dar es Salaam and Mwanza regions of Tanzania.For over 100 years the Aga Khan Development Network (AKDN) has worked to ensure that students of all ages have access to quality learning opportunities.
+                        </ReadMore>
+                    </p>
+                </div>
+            </div>
+            <div className="md:flex mt-6">
+                <div className="md:w-2/3 w-full">
+                    <p className="font-normal text-base text-gray-5 uppercase text-start">Continuing Education</p>
+                </div>
+                <div className="md:w-3/4 w-full">
+                    <p className="text-gray-5 font-normal text-xl">
+                        <ReadMore className="text-gray-5 font-normal text-xl">
+                            Globally, the AKDN's activities in early childhood development provide 750,000 children aged pre-natal-8 with quality early learning opportunities, annually. The project is a joint initiative led by the Aga Khan Health Services Tanzania (AKHST) together with the Aga Khan Foundation Tanzania (AKFT),It aims to strengthen and expand the quality, access, and capacity of cancer care services across the cancer continuum in Dar es Salaam and Mwanza regions of Tanzania.For over 100 years the Aga Khan Development Network (AKDN) has worked to ensure that students of all ages have access to quality learning opportunities.
+                        </ReadMore>
+                    </p>
                 </div>
             </div>
         </section>
