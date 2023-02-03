7bd9472d8324f7061d20ece6b9c191b4e4414ed1
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-02-03 15:08:54 +0500

    new

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -62,7 +62,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl h-full w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl h-76 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
@@ -106,7 +106,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl h-76 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source  src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
@@ -138,7 +138,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl h-76 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
