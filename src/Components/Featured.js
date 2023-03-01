9a24b17c44954f9c4350e455b4ce7ea386570084
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-01 12:48:27 +0500

    new

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -78,7 +78,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl 2xl:h-95 xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source onClick={togglevideo} src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
@@ -93,7 +93,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 w-full flex flex-col text-center items-center">
                                         <img className="w-full" src={require('./assets/Rectangle 1450.png')}></img>
-                                        <audio onClick={toggleaudio} className="absolute xl:top-64 lg:top-48 bottom-40 xl:h-15 lg:h-14 w-full" controls="transparent">
+                                        <audio onClick={toggleaudio} className="absolute 2xl:top-80 xl:top-64 lg:top-48 bottom-40 xl:h-15 lg:h-14 w-full" controls="transparent">
                                             <source className="rounded-none" src={require('./assets/Free_Test_Data_500KB_MP3.mp3')} type="audio/mpeg" />
                                             Your browser does not support the audio element.
                                         </audio>
@@ -122,7 +122,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl 2xl:h-95 xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source onClick={togglevideo} src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
@@ -137,7 +137,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 w-full flex flex-col text-center items-center">
                                         <img className="w-full" src={require('./assets/Rectangle 1450.png')}></img>
-                                        <audio className="absolute xl:top-64 lg:top-48 bottom-40 xl:h-15 lg:h-14 w-full" controls>
+                                        <audio className="absolute 2xl:top-80 xl:top-64 lg:top-48 bottom-40 xl:h-15 lg:h-14 w-full" controls>
                                             <source src={require('./assets/Free_Test_Data_500KB_MP3.mp3')} type="audio/mpeg" />
                                             Your browser does not support the audio element.
                                         </audio>
@@ -154,7 +154,7 @@ function Featured(props) {
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
                                     <div class="p-4 xl:w-full flex flex-col text-center items-center">
                                         {/* <img src={require('./assets/Rectangle 1450.png')}></img> */}
-                                        <video className="rounded-t-2xl xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
+                                        <video className="rounded-t-2xl 2xl:h-95 xl:h-76 lg:h-62 h-99.5 bg-black w-full" controls poster={require('./assets/Rectangle 1450.png')}>
                                             <source onClick={togglevideo} src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" type="video/mp4" />
                                         </video>
                                         <div class="flex-grow mt-7">
