2193339fa7bad037c82d2f5292ae00d457bd5da7
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-03 12:54:42 +0500

    Update Header2.js

--- src/Components/Header2.js
+++ src/Components/Header2.js
@@ -237,7 +237,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout) && (
-                                <nav className="list-none lg:absolute w-full lg:w-auto bg-gray-4 lg:p-5 list6 lg:flex flex-col hidden">
+                                <nav className="list-none lg:absolute w-full lg:top-24 lg:w-auto bg-gray-4 lg:p-5 list6 lg:flex flex-col hidden">
                                     <li>
                                         <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Founder & Chairman</a>
                                     </li>
@@ -274,7 +274,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout1) && (
-                                <nav className="list-none lg:absolute w-full lg:w-auto lg:gap-28 bg-gray-4 lg:p-5 list6 flex lg:flex-row flex-col">
+                                <nav className="list-none lg:absolute w-full lg:w-auto lg:top-24 lg:gap-28 bg-gray-4 lg:p-5 list6 flex lg:flex-row flex-col">
                                     <nav>
                                         <li className="pb-3">
                                             <a className="uppercase text-green-100 font-bold text-xs leading-5">developing human capacity</a>
@@ -373,7 +373,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout2) && (
-                                <nav className="list-none bg-gray-4 lg:p-5 w-full lg:w-auto list6 lg:absolute flex lg:flex-row flex-col">
+                                <nav className="list-none bg-gray-4 lg:p-5 w-full lg:w-auto lg:top-24 list6 lg:absolute flex lg:flex-row flex-col">
                                     <nav className="list-none lg:pe-8">
                                         <li className="pb-3">
                                             <a className="uppercase text-green-100 font-bold text-xs leading-5">Eastern africa</a>
@@ -544,7 +544,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout3) && (
-                                <nav className="list-none lg:absolute list6 bg-gray-4 w-full lg:p-5 lg:w-auto flex lg:flex-row flex-col">
+                                <nav className="list-none lg:absolute list6 bg-gray-4 w-full lg:p-5 lg:top-24 lg:w-auto flex lg:flex-row flex-col">
                                     <nav>
                                         <li className="pb-3">
                                             <a className="uppercase text-green-100 font-bold text-xs leading-5">Our agencies</a>
@@ -612,7 +612,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout4) && (
-                                <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto w-full flex lg:flex-row flex-col">
+                                <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto w-full lg:top-24 flex lg:flex-row flex-col">
                                     <nav>
                                         <li className="pb-3">
                                             <a className="uppercase text-green-100 font-bold text-xs leading-5">What's New</a>
@@ -694,7 +694,7 @@ function Header2() {
                                 </button>
                             </div>
                             {(toggleAbout5) && (
-                                <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto flex w-full flex-col">
+                                <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto flex w-full lg:top-24 flex-col">
                                     <li>
                                         <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Careers</a>
                                     </li>
@@ -778,7 +778,7 @@ function Header2() {
                     </button>
                     <li>
                         <a>
-                            <button onMouseEnter={toggleLang} ref={lang} className="lg:text-gray-5 text-white ms-5 text-sm font-bold uppercase flex px-5 py-2 focus:outline-none">
+                            <button onMouseEnter={toggleLang} ref={lang} className="lg:text-gray-5 lg:py-10 hover:bg-gray-8 text-white ms-5 text-sm font-bold uppercase flex px-5 py-2 focus:outline-none">
                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_4295_1327)">
                                         <path d="M9.99175 1.66663C5.39175 1.66663 1.66675 5.39996 1.66675 9.99996C1.66675 14.6 5.39175 18.3333 9.99175 18.3333C14.6001 18.3333 18.3334 14.6 18.3334 9.99996C18.3334 5.39996 14.6001 1.66663 9.99175 1.66663ZM15.7667 6.66663H13.3084C13.0417 5.62496 12.6584 4.62496 12.1584 3.69996C13.6917 4.22496 14.9667 5.29163 15.7667 6.66663ZM10.0001 3.36663C10.6917 4.36663 11.2334 5.47496 11.5917 6.66663H8.40842C8.76675 5.47496 9.30842 4.36663 10.0001 3.36663ZM3.55008 11.6666C3.41675 11.1333 3.33341 10.575 3.33341 9.99996C3.33341 9.42496 3.41675 8.86663 3.55008 8.33329H6.36675C6.30008 8.88329 6.25008 9.43329 6.25008 9.99996C6.25008 10.5666 6.30008 11.1166 6.36675 11.6666H3.55008ZM4.23341 13.3333H6.69175C6.95841 14.375 7.34175 15.375 7.84175 16.3C6.30841 15.775 5.03341 14.7166 4.23341 13.3333V13.3333ZM6.69175 6.66663H4.23341C5.03341 5.28329 6.30841 4.22496 7.84175 3.69996C7.34175 4.62496 6.95841 5.62496 6.69175 6.66663V6.66663ZM10.0001 16.6333C9.30842 15.6333 8.76675 14.525 8.40842 13.3333H11.5917C11.2334 14.525 10.6917 15.6333 10.0001 16.6333ZM11.9501 11.6666H8.05008C7.97508 11.1166 7.91675 10.5666 7.91675 9.99996C7.91675 9.43329 7.97508 8.87496 8.05008 8.33329H11.9501C12.0251 8.87496 12.0834 9.43329 12.0834 9.99996C12.0834 10.5666 12.0251 11.1166 11.9501 11.6666ZM12.1584 16.3C12.6584 15.375 13.0417 14.375 13.3084 13.3333H15.7667C14.9667 14.7083 13.6917 15.775 12.1584 16.3V16.3ZM13.6334 11.6666C13.7001 11.1166 13.7501 10.5666 13.7501 9.99996C13.7501 9.43329 13.7001 8.88329 13.6334 8.33329H16.4501C16.5834 8.86663 16.6667 9.42496 16.6667 9.99996C16.6667 10.575 16.5834 11.1333 16.4501 11.6666H13.6334Z" fill="#535052" />
@@ -793,7 +793,7 @@ function Header2() {
                             </button>
                         </a>
                         {(toggleLanguage) && (
-                            <nav className="list-none lg:absolute list6 p-5 text-start lg:w-auto w-full flex flex-col bg-gray-4">
+                            <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto flex w-full lg:top-20 flex-col">
                                 <li>
                                     <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Arabic</a>
                                 </li>
