0d21ecbc563167845eb99fe76b7a2a364145aa0c
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-09 19:24:43 +0500

    Update HeroSlider.js

--- src/Components/HeroSlider.js
+++ src/Components/HeroSlider.js
@@ -41,8 +41,8 @@ function HeroSlider(props) {
                             <SwiperSlide>
                                 <img className="w-full" src={require('./assets/Rectangle 275.png')} />
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
-                                    <h3 className="relative px-3 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-3 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
+                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
@@ -50,18 +50,18 @@ function HeroSlider(props) {
                                     <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" type="video/mp4" />
                                 </video>
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
-                                    <h3 className="relative px-3 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-3 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
+                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
                                 <img className="w-full" src={require('./assets/Rectangle 275.png')} />
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
-                                    <h3 className="relative px-3 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-3 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
+                                    <p className="lg:bottom-24 lg:w-2/3 w-full px-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
-                            <div className="w-full relative z-50 2xl:bottom-48 xl:bottom-32 lg:bottom-36 flex 2xl:end-40 lg:end-6 lg:justify-end justify-start">
+                            <div className="w-full relative z-50 2xl:bottom-32 xl:bottom-32 lg:bottom-36 flex 2xl:end-7 lg:end-6 lg:justify-end justify-start">
                                 <div className="lg:flex hidden justify-start">
                                     <button className="seemore swiper-button-prev flex justify-center text-gray-400 transform rotate-180 focus:outline-none sm:px-2">
                                         <svg width="7.41" height="15.19" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
