ab1aa6f91cbc36bd2dff35663e7272f50e35ee17
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-13 18:09:14 +0500

    Update Featured.js

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -36,7 +36,6 @@ function Featured(props) {
                             breakpoints={{
                                 0: {
                                     slidesPerView: 1,
-                                    spaceBetween: 0
                                 },
                                 1024: {
                                     slidesPerView: 3,
@@ -55,7 +54,6 @@ function Featured(props) {
                             }}
                             modules={[Pagination, Navigation]}
                             className="mySwiper"
-                            centeredSlides={true}
                         >
                             <SwiperSlide>
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
