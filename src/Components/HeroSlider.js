8cc7b453dcc56c00e9e613248889e2cd8d2c1fad
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-13 15:51:40 +0500

    new

--- src/Components/HeroSlider.js
+++ src/Components/HeroSlider.js
@@ -21,7 +21,7 @@ function HeroSlider(props) {
     return (
         <section>
             <div className="py-10">
-                <div className="w-full lg:px-0 px-4">
+                <div className="w-full lg:px-0">
                     <>
                         <Swiper
                             pagination={{
@@ -31,7 +31,7 @@ function HeroSlider(props) {
                             }}
                             breakpoints={{
                                 0: {
-                                    slidesPerView: 1.25,
+                                    slidesPerView: 1.05,
                                     spaceBetween: 20
                                 },
                                 1024: {
@@ -50,7 +50,7 @@ function HeroSlider(props) {
                                 <img className="w-full" src={require('./assets/Rectangle 275.png')} />
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
                                     <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 lg:py-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 py-2 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
@@ -59,17 +59,17 @@ function HeroSlider(props) {
                                 </video>
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
                                     <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 lg:py-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 py-2 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
                                 <img className="w-full" src={require('./assets/Rectangle 275.png')} />
                                 <div className="lg:bg-white bg-blue-30 backdrop-blur-md">
                                     <h3 className="relative px-4 lg:text-white text-gray-5 lg:bottom-28 lg:font-medium lg:text-4xl text-sm font-bold">AWARDING SUSTAINABLE ARCHITECTURE</h3>
-                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 lg:py-4 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
+                                    <p className="xl:bottom-28 lg:bottom-32 lg:w-2/3 w-full px-4 py-2 lg:text-white text-gray-5 text-sm font-normal relative">"Sustainable architecture" is now well known, especially as a reaction to the risks inherent in climate change, but the Aga Khan Award for Architecture has been giving out its prize to projects that are good for people and the planet for at least 40 years.</p>
                                 </div>
                             </SwiperSlide>
-                            <div className="w-full relative z-50 2xl:bottom-32 xl:bottom-40 lg:bottom-40 flex 2xl:end-7 lg:end-6 lg:justify-end justify-start">
+                            <div className="w-full relative z-50 2xl:bottom-40 xl:bottom-40 lg:bottom-40 flex 2xl:end-7 lg:end-6 lg:justify-end justify-start">
                                 <div className="lg:flex hidden justify-start">
                                     <button className="seemore swiper-button-prev flex justify-center text-gray-400 transform rotate-180 focus:outline-none sm:px-2">
                                         <svg width="7.41" height="15.19" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -77,8 +77,8 @@ function HeroSlider(props) {
                                         </svg>
                                     </button>
                                 </div>
-                                <div className="flex relative lg:mt-0 mt-2 justify-center">
-                                    <div class="flex items-center gap-2 swiper-pagination5">
+                                <div className="lg:px-0 px-2 flex relative w-full lg:mt-0 mt-2 justify-end">
+                                    <div class="flex items-center lg:w-1/5 w-full gap-2 swiper-pagination5">
                                     </div>
                                 </div>
                                 <div className="lg:flex hidden justify-end">
