5689f5fcb0e10eaf25b832b4fbdeeaed05a96f4e
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-13 18:00:16 +0500

    Update Featured.js

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -56,7 +56,6 @@ function Featured(props) {
                             modules={[Pagination, Navigation]}
                             className="mySwiper"
                             centeredSlides={true}
-                            loop={true}
                         >
                             <SwiperSlide>
                                 <div class="flex flex-wrap md:-m-4 -mx-4 -mb-10 md:space-y-0 space-y-6">
