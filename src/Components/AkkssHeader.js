a2e071e716090520c86da8ee4904b27ebde99b5e
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-14 15:42:07 +0500

    new

--- src/Components/AkkssHeader.js
+++ src/Components/AkkssHeader.js
@@ -235,16 +235,16 @@ function AkhssHeader() {
     return (
         <section className="w-full fixed z-50">
             <div className="flex bg-white lg:border">
-                <div className="filter lg:drop-shadow-3xl relative lg:w-5/12 w-11/12">
-                    <div className="clip h-full">
-                        <div className="inline-flex items-center justify-center lg:relative lg:top-8">
+                <div className="filter lg:drop-shadow-3xl relative lg:w-2/5 w-11/12">
+                    <div className="clip lg:absolute w-full lg:h-40 h-full">
+                        <div className="flex items-center relative lg:top-10">
                             <img className="lg:w-18" src={require('./assets/AKH_Logo_Small 1.png')}></img>
-                            <p className="xl:font-extrabold font-bold self-center xl:text-2.5xl sm:text-xl text-lg">Aga Khan Health Services</p>
+                            <p className="lg:font-extrabold font-bold self-center xl:text-2.5xl sm:text-xl text-lg">Aga Khan Health Services</p>
                         </div>
                     </div>
                 </div>
                 {(toggleMenu || screenWidth > 1023) && (
-                    <div className="xl:w-2/3 lg:w-4/5 lg:relative absolute w-full lg:bg-transparent bg-red-600 bg-opacity-80 flex lg:flex-wrap flex-wrap-reverse">
+                    <div className="lg:w-2/3 lg:relative absolute w-full lg:bg-transparent bg-red-600 bg-opacity-80 flex lg:flex-wrap flex-wrap-reverse">
                         <div className="w-full h-auto">
                             <nav className="list-none lg:bg-gray-100 border lg:border-gray-200 items-center lg:flex-row flex-col gap-3 flex p-4">
                                 <li>
@@ -262,15 +262,15 @@ function AkhssHeader() {
                                 <li>
                                     <a className="font-semibold text-sm lg:text-black text-white">Research</a>
                                 </li>
-                                <label className="w-2/3 lg:flex hidden justify-end">
-                                    <svg className="absolute top-7 end-12" width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <label className="w-full lg:flex hidden justify-end">
+                                    <svg className="absolute top-7 end-8" width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M14.7584 13.3646L11.925 10.5834C12.8392 9.43682 13.3352 8.02095 13.3334 6.56335C13.3334 5.26524 12.9424 3.99628 12.2098 2.91695C11.4773 1.83761 10.4361 0.996372 9.21792 0.499608C7.99974 0.00284377 6.6593 -0.127132 5.36609 0.126116C4.07287 0.379364 2.88498 1.00446 1.95263 1.92236C1.02028 2.84026 0.385337 4.00974 0.128102 5.2829C-0.129134 6.55607 0.00288855 7.87573 0.507474 9.07503C1.01206 10.2743 1.86655 11.2994 2.96288 12.0206C4.05921 12.7418 5.34815 13.1267 6.66669 13.1267C8.14724 13.1285 9.58541 12.6402 10.75 11.7402L13.575 14.5296C13.6525 14.6065 13.7447 14.6675 13.8462 14.7092C13.9478 14.7508 14.0567 14.7723 14.1667 14.7723C14.2767 14.7723 14.3856 14.7508 14.4872 14.7092C14.5887 14.6675 14.6809 14.6065 14.7584 14.5296C14.8365 14.4533 14.8985 14.3626 14.9408 14.2626C14.9831 14.1626 15.0049 14.0554 15.0049 13.9471C15.0049 13.8388 14.9831 13.7316 14.9408 13.6316C14.8985 13.5316 14.8365 13.4409 14.7584 13.3646ZM1.66667 6.56335C1.66667 5.58977 1.95992 4.63805 2.50933 3.82855C3.05874 3.01905 3.83963 2.38812 4.75327 2.01554C5.6669 1.64297 6.67224 1.54549 7.64215 1.73542C8.61205 1.92536 9.50297 2.39418 10.2022 3.08261C10.9015 3.77103 11.3777 4.64814 11.5706 5.60301C11.7636 6.55789 11.6645 7.54764 11.2861 8.44711C10.9077 9.34658 10.2668 10.1154 9.44455 10.6563C8.6223 11.1972 7.6556 11.4859 6.66669 11.4859C5.3406 11.4859 4.06883 10.9672 3.13115 10.0441C2.19346 9.12094 1.66667 7.86888 1.66667 6.56335Z" fill="#A6A6A6" />
                                     </svg>
-                                    <input type="email" id="search" name="Search" className="w-full h-9 bg-white border border-gray-200 text-sm font-normal leading-8" />
+                                    <input type="email" id="search" name="Search" className="w-96 h-9 bg-white border border-gray-200 text-sm font-normal leading-8" />
                                 </label>
                             </nav>
                         </div>
-                        <nav className="list-none w-full items-center bg-white lg:flex-row flex-col h-14 xl:py-4 lg:px-0 px-4 gap-3 flex">
+                        <nav className="list-none w-full items-center bg-white lg:flex-row flex-col h-18 xl:py-4 lg:px-0 px-4 gap-3 flex">
                             <button onClick={toggleNav} className="p-8 lg:hidden flex w-auto justify-end">
                                 <svg width="31" height="32" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <rect x="5.87109" y="0.786377" width="36" height="6" rx="3" transform="rotate(48.7268 5.87109 0.786377)" fill="#FFF9F9" />
--- src/Components/Header2.js
+++ src/Components/Header2.js
@@ -210,15 +210,6 @@ function Header2() {
         }
     }, [toggleAbout5])
 
-    // const Transition = forwardRef(function Transition(
-    //     props: TransitionProps & {
-    //         children: React.ReactElement;
-    //     },
-    //     ref: React.Ref<unknown>,
-    // ) {
-    //     return <Slide direction="down" ref={ref} {...props} />;
-    // });
-
     return (
         <header className="w-full z-50 fixed">
             <div className="flex w-full lg:flex-wrap lg:h-28 h-18 flex-wrap-reverse justify-between items-center absolute bg-white">
@@ -227,14 +218,14 @@ function Header2() {
                 <button onClick={toggleNav} className="flex items-center lg:order-1 order-0 focus:outline-none p-2 ms-3 text-sm font-semibold uppercase border-white border-2 px-6 py-3 text-gray-5 lg:hidden">
                     <img src={require('./assets/Menu.png')}></img>
                 </button>
-                {/* <Dialog
-                    fullScreen
-                    open={toggleNav}
-                    onClose={handleClose}
-                > */}
-                    {(toggleMenu || screenWidth > 1024) && (
-                        <div className="list7 list-none lg:bg-white h-full gap-3 lg:px-0 flex lg:flex-row flex-col order-0 lg:justify-center justify-start">
-                            <div className="flex lg:flex-row flex-col xl:gap-3 lg:bg-white bg-green-20 overflow-visible  lg:px-0 px-8">
+                {(toggleMenu || screenWidth > 1024) && (
+                    <div className="list7 list-none lg:bg-white h-full gap-3 lg:px-0 flex lg:flex-row flex-col order-0 lg:justify-center justify-start">
+                        <div className="flex lg:flex-row flex-col xl:gap-3 lg:bg-white bg-green-20 lg:px-0 px-8">
+                            <Dialog
+                                fullScreen
+                                open={toggleMenu}
+                                className="list-none flex lg:flex-row flex-col order-0 lg:justify-center justify-start lg:bg-white bg-green-20"
+                            >
                                 <div className="lg:hidden lg:h-28 h-18 w-full flex flex-wrap justify-between content-center items-center">
                                     <img href="Akdn" className="order-1 flex w-24 h-8" src={require('./assets/White.png')}></img>
                                     <button onClick={handleClose} className="flex items-center order-0 focus:outline-none text-sm font-semibold uppercase py-3 text-gray-5 lg:hidden">
@@ -247,7 +238,7 @@ function Header2() {
                                         </svg>
                                     </button>
                                 </div>
-                                <li className="hover:bg-gray-8" onMouseEnter={toggleAbt} ref={ref}>
+                                <li className="lg:hover:bg-gray-8" onMouseEnter={toggleAbt} ref={ref}>
                                     <div className="flex">
                                         <a className="lg:w-auto w-1/2 h-full cursor-pointer uppercase lg:block hidden py-3 lg:py-10 px-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Who We Are</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Who We Are</a>
@@ -284,7 +275,7 @@ function Header2() {
                                         </nav>
                                     )}
                                 </li>
-                                <li className="hover:bg-gray-8" onMouseEnter={toggleAbt1} ref={ref1}>
+                                <li className="lg:hover:bg-gray-8" onMouseEnter={toggleAbt1} ref={ref1}>
                                     <div className="flex">
                                         <a className="hidden lg:w-auto w-1/2 cursor-pointer uppercase lg:block py-3 lg:py-10 hover:bg-gray-8 px-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">What We Do</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">What We Do</a>
@@ -383,7 +374,7 @@ function Header2() {
                                         </nav>
                                     )}
                                 </li>
-                                <li onMouseEnter={toggleAbt2} className="hover:bg-gray-8" ref={ref2}>
+                                <li onMouseEnter={toggleAbt2} className="lg:hover:bg-gray-8" ref={ref2}>
                                     <div className="flex">
                                         <a className="hidden lg:w-auto w-1/2 cursor-pointer uppercase lg:block py-3 lg:py-10 px-2 hover:bg-gray-8 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">How We Work</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">How We Work</a>
@@ -554,7 +545,7 @@ function Header2() {
                                         </nav>
                                     )}
                                 </li>
-                                <li className="hover:bg-gray-8" onMouseEnter={toggleAbt3} ref={ref3}>
+                                <li className="lg:hover:bg-gray-8" onMouseEnter={toggleAbt3} ref={ref3}>
                                     <div className="flex">
                                         <a className="hidden lg:w-auto w-1/2 cursor-pointer uppercase lg:block py-3 lg:py-10 px-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Where We Work</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Where We Work</a>
@@ -622,7 +613,7 @@ function Header2() {
                                         </nav>
                                     )}
                                 </li>
-                                <li className="hover:bg-gray-8" onMouseEnter={toggleAbt4} ref={ref4}>
+                                <li className="lg:hover:bg-gray-8" onMouseEnter={toggleAbt4} ref={ref4}>
                                     <div className="flex">
                                         <a className="hidden lg:w-auto w-1/2 cursor-pointer uppercase lg:block py-3 lg:py-10 px-2 hover:bg-gray-8 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">RESOURCES & MEDIA</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">RESOURCES & MEDIA</a>
@@ -704,7 +695,7 @@ function Header2() {
                                         </nav>
                                     )}
                                 </li>
-                                <li className="hover:bg-gray-8" onMouseEnter={toggleAbt5} ref={ref5}>
+                                <li className="lg:hover:bg-gray-8" onMouseEnter={toggleAbt5} ref={ref5}>
                                     <div className="flex">
                                         <a className="hidden lg:w-auto w-1/2 cursor-pointer uppercase lg:block py-3 lg:py-10 px-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Get Involved</a>
                                         <a className="lg:hidden lg:w-auto w-1/2 cursor-pointer uppercase block py-3 lg:py-2 font-bold text-xs leading-7 lg:text-gray-5 text-white focus:outline-none">Get Involved</a>
@@ -780,62 +771,62 @@ function Header2() {
                                         )}
                                     </li>
                                 </li>
-                            </div>
-                        </div >
-                    )}
-                    {/* </Dialog> */}
-                    <div className="lg:hidden flex me-10 lg:order-1 order-2">
-                        <button className="font-bold uppercase ms-3 py-2 focus:outline-none">
-                            <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M15.3604 9.16667C15.3604 12.2963 12.8234 14.8333 9.69377 14.8333C6.56415 14.8333 4.0271 12.2963 4.0271 9.16667C4.0271 6.03705 6.56415 3.5 9.69377 3.5C12.8234 3.5 15.3604 6.03705 15.3604 9.16667Z" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M18.0271 17.5L14.4021 13.875" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="list-none relative text-center lg:flex hidden md:bg-transparent lg:order-1 order-2 md:p-0 p-5 md:w-52 h-full w-full bg-white">
-                        <button className="font-bold uppercase ms-3 py-2 hover:bg-gray-8 px-2 focus:outline-none">
-                            <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M15.3604 9.16667C15.3604 12.2963 12.8234 14.8333 9.69377 14.8333C6.56415 14.8333 4.0271 12.2963 4.0271 9.16667C4.0271 6.03705 6.56415 3.5 9.69377 3.5C12.8234 3.5 15.3604 6.03705 15.3604 9.16667Z" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M18.0271 17.5L14.4021 13.875" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
-                        <li className="hover:bg-gray-8" onMouseEnter={toggleLang} ref={lang}>
-                            <a>
-                                <button className="lg:text-gray-5 lg:py-11 text-white ms-5 text-sm font-bold uppercase flex px-5 py-2 focus:outline-none">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_4295_1327)">
-                                            <path d="M9.99175 1.66663C5.39175 1.66663 1.66675 5.39996 1.66675 9.99996C1.66675 14.6 5.39175 18.3333 9.99175 18.3333C14.6001 18.3333 18.3334 14.6 18.3334 9.99996C18.3334 5.39996 14.6001 1.66663 9.99175 1.66663ZM15.7667 6.66663H13.3084C13.0417 5.62496 12.6584 4.62496 12.1584 3.69996C13.6917 4.22496 14.9667 5.29163 15.7667 6.66663ZM10.0001 3.36663C10.6917 4.36663 11.2334 5.47496 11.5917 6.66663H8.40842C8.76675 5.47496 9.30842 4.36663 10.0001 3.36663ZM3.55008 11.6666C3.41675 11.1333 3.33341 10.575 3.33341 9.99996C3.33341 9.42496 3.41675 8.86663 3.55008 8.33329H6.36675C6.30008 8.88329 6.25008 9.43329 6.25008 9.99996C6.25008 10.5666 6.30008 11.1166 6.36675 11.6666H3.55008ZM4.23341 13.3333H6.69175C6.95841 14.375 7.34175 15.375 7.84175 16.3C6.30841 15.775 5.03341 14.7166 4.23341 13.3333V13.3333ZM6.69175 6.66663H4.23341C5.03341 5.28329 6.30841 4.22496 7.84175 3.69996C7.34175 4.62496 6.95841 5.62496 6.69175 6.66663V6.66663ZM10.0001 16.6333C9.30842 15.6333 8.76675 14.525 8.40842 13.3333H11.5917C11.2334 14.525 10.6917 15.6333 10.0001 16.6333ZM11.9501 11.6666H8.05008C7.97508 11.1166 7.91675 10.5666 7.91675 9.99996C7.91675 9.43329 7.97508 8.87496 8.05008 8.33329H11.9501C12.0251 8.87496 12.0834 9.43329 12.0834 9.99996C12.0834 10.5666 12.0251 11.1166 11.9501 11.6666ZM12.1584 16.3C12.6584 15.375 13.0417 14.375 13.3084 13.3333H15.7667C14.9667 14.7083 13.6917 15.775 12.1584 16.3V16.3ZM13.6334 11.6666C13.7001 11.1166 13.7501 10.5666 13.7501 9.99996C13.7501 9.43329 13.7001 8.88329 13.6334 8.33329H16.4501C16.5834 8.86663 16.6667 9.42496 16.6667 9.99996C16.6667 10.575 16.5834 11.1333 16.4501 11.6666H13.6334Z" fill="#535052" />
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0_4295_1327">
-                                                <rect width="20" height="20" fill="white" />
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                    en
-                                </button>
-                            </a>
-                            {(toggleLanguage) && (
-                                <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto flex w-full lg:top-20 flex-col">
-                                    <li>
-                                        <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Arabic</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">English</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Français</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Português</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Pyccкий</a>
-                                    </li>
-                                </nav>
-                            )}
-                        </li>
-                    </div>
+                            </Dialog>
+                        </div>
+                    </div >
+                )}
+                <div className="lg:hidden flex me-10 lg:order-1 order-2">
+                    <button className="font-bold uppercase ms-3 py-2 focus:outline-none">
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.3604 9.16667C15.3604 12.2963 12.8234 14.8333 9.69377 14.8333C6.56415 14.8333 4.0271 12.2963 4.0271 9.16667C4.0271 6.03705 6.56415 3.5 9.69377 3.5C12.8234 3.5 15.3604 6.03705 15.3604 9.16667Z" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.0271 17.5L14.4021 13.875" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="list-none relative text-center lg:flex hidden md:bg-transparent lg:order-1 order-2 md:p-0 p-5 md:w-52 h-full w-full bg-white">
+                    <button className="font-bold uppercase ms-3 py-2 lg:hover:bg-gray-8 px-2 focus:outline-none">
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.3604 9.16667C15.3604 12.2963 12.8234 14.8333 9.69377 14.8333C6.56415 14.8333 4.0271 12.2963 4.0271 9.16667C4.0271 6.03705 6.56415 3.5 9.69377 3.5C12.8234 3.5 15.3604 6.03705 15.3604 9.16667Z" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.0271 17.5L14.4021 13.875" stroke="#535052" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                    <li className="lg:hover:bg-gray-8" onMouseEnter={toggleLang} ref={lang}>
+                        <a>
+                            <button className="lg:text-gray-5 lg:py-11 text-white ms-5 text-sm font-bold uppercase flex px-5 py-2 focus:outline-none">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_4295_1327)">
+                                        <path d="M9.99175 1.66663C5.39175 1.66663 1.66675 5.39996 1.66675 9.99996C1.66675 14.6 5.39175 18.3333 9.99175 18.3333C14.6001 18.3333 18.3334 14.6 18.3334 9.99996C18.3334 5.39996 14.6001 1.66663 9.99175 1.66663ZM15.7667 6.66663H13.3084C13.0417 5.62496 12.6584 4.62496 12.1584 3.69996C13.6917 4.22496 14.9667 5.29163 15.7667 6.66663ZM10.0001 3.36663C10.6917 4.36663 11.2334 5.47496 11.5917 6.66663H8.40842C8.76675 5.47496 9.30842 4.36663 10.0001 3.36663ZM3.55008 11.6666C3.41675 11.1333 3.33341 10.575 3.33341 9.99996C3.33341 9.42496 3.41675 8.86663 3.55008 8.33329H6.36675C6.30008 8.88329 6.25008 9.43329 6.25008 9.99996C6.25008 10.5666 6.30008 11.1166 6.36675 11.6666H3.55008ZM4.23341 13.3333H6.69175C6.95841 14.375 7.34175 15.375 7.84175 16.3C6.30841 15.775 5.03341 14.7166 4.23341 13.3333V13.3333ZM6.69175 6.66663H4.23341C5.03341 5.28329 6.30841 4.22496 7.84175 3.69996C7.34175 4.62496 6.95841 5.62496 6.69175 6.66663V6.66663ZM10.0001 16.6333C9.30842 15.6333 8.76675 14.525 8.40842 13.3333H11.5917C11.2334 14.525 10.6917 15.6333 10.0001 16.6333ZM11.9501 11.6666H8.05008C7.97508 11.1166 7.91675 10.5666 7.91675 9.99996C7.91675 9.43329 7.97508 8.87496 8.05008 8.33329H11.9501C12.0251 8.87496 12.0834 9.43329 12.0834 9.99996C12.0834 10.5666 12.0251 11.1166 11.9501 11.6666ZM12.1584 16.3C12.6584 15.375 13.0417 14.375 13.3084 13.3333H15.7667C14.9667 14.7083 13.6917 15.775 12.1584 16.3V16.3ZM13.6334 11.6666C13.7001 11.1166 13.7501 10.5666 13.7501 9.99996C13.7501 9.43329 13.7001 8.88329 13.6334 8.33329H16.4501C16.5834 8.86663 16.6667 9.42496 16.6667 9.99996C16.6667 10.575 16.5834 11.1333 16.4501 11.6666H13.6334Z" fill="#535052" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_4295_1327">
+                                            <rect width="20" height="20" fill="white" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                                en
+                            </button>
+                        </a>
+                        {(toggleLanguage) && (
+                            <nav className="list-none lg:absolute list6 bg-gray-4 lg:p-5 lg:w-auto flex w-full lg:top-20 flex-col">
+                                <li>
+                                    <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Arabic</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">English</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Français</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Português</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-white font-normal text-sm pb-2 inline-flex">Pyccкий</a>
+                                </li>
+                            </nav>
+                        )}
+                    </li>
+                </div>
             </div >
         </header >
     )
