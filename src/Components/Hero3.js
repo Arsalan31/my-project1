8ac765dc91c8b98d6c18cb2902074c6fc3c3eafe
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-15 18:34:27 +0500

    new

---
+++ src/Components/Hero3.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './style.css';
+
+
+function Hero3() {
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Hero3;
--- src/Slack.js
+++ src/Slack.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import Hero3 from "./Components/Hero3";
 
 
 function Slack() {
     const [isRtl, setIsRtl] = useState(false)
     return (
         <React.Fragment>
-
+            <Hero3 />
         </React.Fragment>
     )
 
