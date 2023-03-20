3eb4d43f66193b9c93eba31645927c3ebc68ae2b
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-20 18:10:15 +0500

    Create Header3.js

---
+++ src/Components/Header3.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef, useState } from "react";
+import './style.css';
+
+
+function Header3() {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default Header3;
