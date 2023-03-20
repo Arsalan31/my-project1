2f53d3cf296256562a4b840cb2d1b951e9fab3be
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-20 18:10:39 +0500

    Update Slack.js

--- src/Slack.js
+++ src/Slack.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Header3 from "./Components/Header3";
 import Hero3 from "./Components/Hero3";
 import Solutions from "./Components/Solutions";
 
@@ -8,6 +9,7 @@ function Slack() {
     return (
         <React.Fragment>
             <div dir={isRtl ? "rtl" : "ltr"}>
+                <Header3 />
                 <Hero3 isRtl={isRtl} setIsRtl={setIsRtl} />
                 <Solutions />
             </div>
