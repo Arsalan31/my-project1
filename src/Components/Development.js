8d423e36c5cbd6ef84ca63ca287aaaa6f8bb0671
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-21 14:10:46 +0500

    new

---
+++ src/Components/Development.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './style.css';
+
+
+function Development(){
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Development;
