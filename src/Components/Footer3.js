1c7d440b2105f70d116c76d48343a0e63e459437
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-24 17:07:23 +0500

    new

--- src/Components/Footer3.js
+++ src/Components/Footer3.js
@@ -1,3 +1,4 @@
+import { hover } from "@testing-library/user-event/dist/hover";
 import React from "react"
 import './style.css';
 
@@ -31,12 +32,12 @@ function Footer3() {
                     </div>
                     <div className="flex justify-center items-start gap-4 pt-5 lg:w-1/2">
                         <a href="#">
-                            <svg className="In" width="18" height="18" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="icon" width="18" height="18" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M4.25 18.2658H0.5V5.14073H4.25V18.2658ZM2.49123 4.33638C1.51998 4.33638 0.734379 3.54887 0.734379 2.57761C0.734379 1.60636 1.52189 0.818848 2.49123 0.818848C3.46249 0.820698 4.25 1.60821 4.25 2.57761C4.25 3.54887 3.46249 4.33638 2.49123 4.33638ZM17.375 18.2658H13.625V10.1508C13.625 9.20011 13.3531 8.53454 12.185 8.53454C10.2481 8.53454 9.87497 10.1508 9.87497 10.1508V18.2658H6.12497V5.14073H9.87497V6.39509C10.4112 5.9845 11.7499 5.14258 13.625 5.14258C14.84 5.14258 17.375 5.87008 17.375 10.2651V18.2658Z" fill="#FFFFFF" />
                             </svg>
                         </a>
                         <a href="#">
-                            <svg className="fb" width="10" height="18" viewBox="0 0 10 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="icon" width="10" height="18" viewBox="0 0 10 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8.73041 9.52795H6.29003C6.29003 13.4269 6.29003 18.2261 6.29003 18.2261H2.67386C2.67386 18.2261 2.67386 13.4734 2.67386 9.52795H0.954895V6.45376H2.67386V4.46532C2.67386 3.04121 3.35057 0.815918 6.32326 0.815918L9.00291 0.82619V3.81036C9.00291 3.81036 7.37458 3.81036 7.05797 3.81036C6.74137 3.81036 6.29124 3.96866 6.29124 4.64779V6.45436H9.04641L8.73041 9.52795Z" fill="#FFFFFF" />
                             </svg>
                         </a>
@@ -44,7 +45,7 @@ function Footer3() {
                 </div>
             </div>
             <div className="bg-blue-90 h-13 flex justify-center items-center">
-                <p className="text-white  text-xs">© 2022 All rights reserved. Rivers &amp; Roads Organizational Development</p>
+                <p className="text-white text-center text-xs">© 2022 All rights reserved. Rivers &amp; Roads Organizational Development</p>
             </div>
         </footer>
     )
--- src/Components/Header3.js
+++ src/Components/Header3.js
@@ -8,46 +8,73 @@ import './style.css';
 function Header3() {
 
     const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const [navbar, setNavbar] = useState(false)
 
     const toggleNav = () => {
         setToggleMenu(!toggleMenu)
     }
 
+    useEffect(() => {
+
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+
+    }, [])
+
+    const changeBackground = () => {
+        if (window.scrollY >= 160) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground)
 
     return (
-        <header className="w-full lg:fixed">
-            <div className="lg:bg-transparent bg-blue-80">
-                <div className="h-24 flex-wrap lg:mx-auto lg:container items-center justify-center flex">
-                    <button onClick={toggleNav} className="lg:hidden flex justify-end items-center p-2 ms-3 text-sm hover:text-green-40">
+        <header className="w-full fixed">
+            <nav className={navbar ? 'Navbar active' : 'Navbar'}>
+                <div className="h-36 flex-wrap lg:mx-auto lg:container items-center justify-center flex">
+                    <button onClick={toggleNav} className="icon lg:hidden flex justify-end items-center p-2 ms-3 text-sm hover:text-green-40">
                         <FontAwesomeIcon icon={faBars} style={{ color: "#ffffff", }} />
                     </button>
-                    <a className="lg:flex hidden" href="rivers-roads">
+                    <a className="lg:flex hidden h-full" href="rivers-roads">
                         <img className="h-full" src={require('./assets/RVR-sticky.png')} />
                     </a>
                     <a href="rivers-roads" className="lg:hidden flex justify-center w-3/4 h-full">
                         <img className="h-full" src={require('./assets/RVR-sticky.png')} />
                     </a>
-                    {(toggleMenu) && (
-                        <div className="lg:px-0 px-5 list-none justify-center flex lg:flex-row flex-col lg:bg-transparent bg-blue-80 lg:w-2/3 w-full lg:justify-end lg:items-center">
-                            <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
-                                <a href="#Solution" className="text-white text-base font-medium px-3">Solutions</a>
-                            </li>
-                            <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
-                                <a className="text-white text-base font-medium px-3">Our Process</a>
-                            </li>
-                            <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
-                                <a className="text-white text-base font-medium px-3">Organizational Development</a>
-                            </li>
-                            <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
-                                <a className="text-white text-base font-medium px-3">About</a>
-                            </li>
-                            <li className="lg:py-0 py-3">
-                                <a className="text-white text-base font-medium px-3">Contact</a>
-                            </li>
+                    {(toggleMenu || screenWidth > 1023) && (
+                        <div className=" lg:bg-transparent bg-blue-80 lg:w-2/3 w-full ">
+                            <div className="mx-auto container lg:px-0 px-5 list-none justify-center flex lg:flex-row flex-col lg:justify-end lg:items-center">
+                                <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
+                                    <a href="#Solution" className="text-white text-base font-medium px-3">Solutions</a>
+                                </li>
+                                <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
+                                    <a className="text-white text-base font-medium px-3">Our Process</a>
+                                </li>
+                                <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
+                                    <a className="text-white text-base font-medium px-3">Organizational Development</a>
+                                </li>
+                                <li className="lg:py-0 py-3 lg:border-b-0 border-b border-white">
+                                    <a className="text-white text-base font-medium px-3">About</a>
+                                </li>
+                                <li className="lg:py-0 py-3">
+                                    <a className="text-white text-base font-medium px-3">Contact</a>
+                                </li>
+                            </div>
                         </div>
                     )}
                 </div>
-            </div>
+            </nav>
         </header>
     )
 }
