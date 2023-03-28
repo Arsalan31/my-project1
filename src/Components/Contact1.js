5c763321ca492fd6047258691e9a056a595c2b02
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-28 15:29:42 +0500

    new

---
+++ src/Components/Contact1.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './style.css';
+
+
+function Contact1() {
+    return (
+        <section className="bg-2">
+
+        </section>
+    )
+}
+
+
+export default Contact1;
