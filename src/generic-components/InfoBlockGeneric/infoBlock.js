f1dcc736ff9f1e2ca8506e0bfccba6ff6945165b
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-29 16:57:59 +0500

    Update infoBlock.js

--- src/generic-components/InfoBlockGeneric/infoBlock.js
+++ src/generic-components/InfoBlockGeneric/infoBlock.js
@@ -1,10 +1,14 @@
 import Sample from '../components/Sample/Sample'
 import About from './components/Sample/About'
+import Hero from './components/Sample/Hero'
+import Solutions from './components/Sample/Solutions'
 
 const InfoBlock = () => {
   return (
     <>
+      <Hero />
       <Sample direction='ltr' />
+      <Solutions />
       <About />
     </>
   )
