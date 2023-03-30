36ec86c9594b66fe433a21a650527136c861c86c
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-30 16:25:25 +0500

    Update Footer_rvr.js

--- src/generic-components/InfoBlockGeneric/components/Footer_rvr/Footer_rvr.js
+++ src/generic-components/InfoBlockGeneric/components/Footer_rvr/Footer_rvr.js
@@ -3,43 +3,18 @@ import InfoBlockGeneric from '../../generic-components/InfoBlockGeneric/InfoBloc
 const Footer = (props) => {
 
     let data = {
-        "wysiwygRepeater": [
-            {
-                "wysiwyg": "About",
-                "position": "1x2",
-                "preset": ""
-            },
-            {
-                "wysiwyg": "Rivers & Roads is an organizational development consulting firm that brings a unique expertise to help your organization achieve its goals. We have a complete understanding of the people side of the organization as well as operations to provide executives with solutions to overcome challenges and pain points associated with the intricate dynamics of organizational systems and culture.",
-                "position": "2x1",
-                "preset": ""
-            },
-            {
-                "wysiwyg": "We spend time to understand the financial performance and inner workings of your company so we can align the strategy, structure, and people. Sometimes we will look upstream at where you have been, others, we will look down the road at where you're going, and our commitment is that we meet you with authenticity, passion, and purpose at every turn.",
-                "position": "2x2",
-                "preset": ""
-            },
-        ],
         "mediaRepeater": [
             {
                 "mediaType": "Image",
-                "position": "3x2",
+                "position": "1x1",
                 "preset": "",
                 "image": {
-                    "url": "./static/images/Zavy-Lyndi-2.jpeg",
+                    "url": "./static/images/RVR-sticky.png",
                     "alt": "",
-
                 },
             }
 
 
-        ],
-        "ctaRepeater": [
-            {
-                "ctaLink": "https://www.google.com",
-                "ctaCaption": "READ BIO",
-                "position": "4x2",
-            }
         ],
         "menuRepeater": [
             {
