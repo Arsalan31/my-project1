77f8a8c64f1aa9f18ae5dde6b396fd1126e7a8e9
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-30 20:06:34 +0500

    new

--- src/generic-components/InfoBlockGeneric/components/About_rvr/About_rvr.js
+++ src/generic-components/InfoBlockGeneric/components/About_rvr/About_rvr.js
@@ -7,24 +7,24 @@ const About = (props) => {
             {
                 "wysiwyg": "About",
                 "position": "1x2",
-                "preset": ""
+                "preset": "AboutHead"
             },
             {
                 "wysiwyg": "Rivers & Roads is an organizational development consulting firm that brings a unique expertise to help your organization achieve its goals. We have a complete understanding of the people side of the organization as well as operations to provide executives with solutions to overcome challenges and pain points associated with the intricate dynamics of organizational systems and culture.",
                 "position": "2x1",
-                "preset": ""
+                "preset": "AboutSubHead"
             },
             {
                 "wysiwyg": "We spend time to understand the financial performance and inner workings of your company so we can align the strategy, structure, and people. Sometimes we will look upstream at where you have been, others, we will look down the road at where you're going, and our commitment is that we meet you with authenticity, passion, and purpose at every turn.",
                 "position": "2x2",
-                "preset": ""
+                "preset": "AboutSubHead"
             },
         ],
         "mediaRepeater": [
             {
                 "mediaType": "Image",
                 "position": "3x2",
-                "preset": "",
+                "preset": "Aboutimg",
                 "image": {
                     "url": "./static/images/Zavy-Lyndi-2.jpeg",
                     "alt": "",
@@ -41,37 +41,7 @@ const About = (props) => {
                 "position": "4x2",
             }
         ],
-        "menuRepeater": [
-            {
-                "position": "1x2",
-                "preset": "",
-                "menu": [
-                    {
-                        "title": " SOLUTIONS",
-                        "link": "",
-                        "iconName": "",
-                        "iconUrl": "",
-                    },
-                    {
-                        "title": "OUR PROCESS",
-                        "link": "#",
-                    },
-                    {
-                        "title": "OUR DEVELOPMENT",
-                        "link": "#",
-                    },
-                    {
-                        "title": "ABOUT",
-                        "link": "#",
-                    },
-                    {
-                        "title": "CONTACT",
-                        "link": "#",
-                    },
-                ],
-            },
-            {}
-        ],
+
         "enableHalfWidth": false,
         "backgroundColor": "",
         "backgroundImage": "",
@@ -79,7 +49,7 @@ const About = (props) => {
     }
 
     return (
-        <div>
+        <div className={`${styles.infoBlock}`}>
             <InfoBlockGeneric {...data} direction={props.direction} />
         </div>
     )
--- src/generic-components/InfoBlockGeneric/components/Development_rvr/Development_rvr.js
+++ src/generic-components/InfoBlockGeneric/components/Development_rvr/Development_rvr.js
@@ -7,22 +7,22 @@ const Development = (props) => {
             {
                 "wysiwyg": "WHAT IS ORGANIZATIONAL DEVELOPMENT?",
                 "position": "1x1",
-                "preset": ""
+                "preset": "DevelopmentHead"
             },
             {
                 "wysiwyg": "Organizational Development (OD) seeks to align the strategies, structure, people, and metrics within an organization to achieve greater effectiveness.",
                 "position": "3x1",
-                "preset": ""
+                "preset": "DevelopmentSubHead"
             },
             {
                 "wysiwyg": "The practice of OD is a combination of psychology, human resource management, organizational behavior, adult education, and culture. Its focus on data and research seeks to provide system-wide improvements to an organization's performance.",
                 "position": "3x2",
-                "preset": ""
+                "preset": "DevelopmentSubHead"
             },
             {
                 "wysiwyg": "OD emerged from human relations studies in the 1930s, during which psychologists realized that organizational structures and processes influence worker behavior and motivation.",
                 "position": "3x3",
-                "preset": ""
+                "preset": "DevelopmentSubHead"
             }
         ],
 
