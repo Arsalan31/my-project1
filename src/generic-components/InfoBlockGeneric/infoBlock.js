139de71eb623c3edd05b08e9b2f400ad409bd77c
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-31 15:17:08 +0500

    Update infoBlock.js

--- src/generic-components/InfoBlockGeneric/infoBlock.js
+++ src/generic-components/InfoBlockGeneric/infoBlock.js
@@ -2,6 +2,7 @@ import Sample from '../components/Sample/Sample'
 import Solutioncard from '../QuickLinksGeneric/Components/SolutionCard_rvr/Solutioncard_rvr'
 import About from './components/About_rvr/About_rvr'
 import Development from './components/Development_rvr/Development_rvr'
+import Footer from './components/Footer_rvr/Footer_rvr'
 import Hero from './components/Hero_rvr/Hero_rvr'
 import Solutions from './components/Solution_rvr/Solutions_rvr'
 
@@ -15,6 +16,7 @@ const InfoBlock = () => {
       <Solutioncard direction='ltr' />
       <Development direction='ltr' />
       <About direction='ltr' />
+      <Footer direction='ltr' />
     </>
   )
 }
