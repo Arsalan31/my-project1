297d1c0757be90e802176e92df5a354282763dda
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-31 17:44:40 +0500

    new

--- src/generic-components/InfoBlockGeneric/Wysiwyg/WysiwygImport.js
+++ src/generic-components/InfoBlockGeneric/Wysiwyg/WysiwygImport.js
@@ -1,24 +1,8 @@
 import Sample from "./Presets/Sample/Sample"
-import Herotxt from "./Presets/Hero_rvr/Herotxt"
-import Herotxt1 from "./Presets/Hero_rvr/Herotxt1"
-import SolutionHead from "./Presets/Solutions_rvr/SolutionsHead"
-import SolutionSubHead from "./Presets/Solutions_rvr/SolutionsSubHead"
-import DevelopmentHead from "./Presets/Development_rvr/DevelopmentHead"
-import DevelopmentSubHead from "./Presets/Development_rvr/DevelopmentSubHead"
-import AboutHead from "./Presets/About_rvr/AboutHead"
-import AboutSubHead from "./Presets/About_rvr/AboutSubHead"
-import FooterMenu from "./Presets/Footer_rvr/FooterMenu"
+
 
 
 export default {
     Sample,
-    Herotxt,
-    Herotxt1,
-    SolutionHead,
-    SolutionSubHead,
-    DevelopmentHead,
-    DevelopmentSubHead,
-    AboutHead,
-    AboutSubHead,
-    FooterMenu 
+ 
 }
