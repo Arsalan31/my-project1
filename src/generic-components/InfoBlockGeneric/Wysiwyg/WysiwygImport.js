b01515152ec92fb2f59413aa1a8a47493183f2e5
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-03-31 17:46:46 +0500

    Update WysiwygImport.js

--- src/generic-components/InfoBlockGeneric/Wysiwyg/WysiwygImport.js
+++ src/generic-components/InfoBlockGeneric/Wysiwyg/WysiwygImport.js
@@ -1,8 +1,23 @@
+import AboutHead from "./Presets/AboutHead/AboutHead"
+import AboutSubHead from "./Presets/AboutSubHead/AboutSubHead"
+import DevelopmentHead from "./Presets/DevelopmentHead/DevelopmentHead"
+import DevelopmentSubHead from "./Presets/DevelopmentSubHead/DevelopmentSubHead"
+import Herotxt from "./Presets/Herotxt/Herotxt"
+import Herotxt1 from "./Presets/Herotxt1/Herotxt1"
 import Sample from "./Presets/Sample/Sample"
+import SolutionHead from "./Presets/SolutionsHead/SolutionsHead"
+import SolutionSubHead from "./Presets/SolutionsSubHead/SolutionsSubHead"
 
 
 
 export default {
     Sample,
- 
+    Herotxt,
+    Herotxt1,
+    SolutionHead,
+    SolutionSubHead,
+    DevelopmentHead,
+    DevelopmentSubHead,
+    AboutHead,
+    AboutSubHead,
 }
