d6ab07484eeae3e2d3e6289f4d7bb54bb3a73228
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-04-07 11:58:41 +0500

    Update About_rvr.js

--- src/generic-components/InfoBlockGeneric/components/About_rvr/About_rvr.js
+++ src/generic-components/InfoBlockGeneric/components/About_rvr/About_rvr.js
@@ -21,8 +21,8 @@ const About = (props) => {
             },
             {
                 "wysiwyg": "Lyndi Zavy, M.A., PHR is an organizational development consultant and founder of Rivers & Roads. A creative visionary and strategic thinker, Lyndi is committed to the study of organizations and uses her intellectual curiosity and business acumen to envision new ways to work and contribute to broader society. She has extensive experience in developing and managing all aspects of an organization's quality and business improvement efforts such as developing and administering programs, training and coaching employees, and facilitating organization- wide change. Lyndi is a respected and sought - after speaker on the topics of strategic and operational planning, the impacts of work / life balance, and program evaluation.She holds a BA in psychology from the University of Oklahoma and Master's in Industrial and Organizational Psychology from Middle Tennessee State University.Lyndi is passionate about giving back to the community through participation in several non - profit boards and committees.She and her husband have two precocious kids, a beloved rescue dog, and a leopard gecko.When working with Lyndi, you can expect song lyrics, movie quotes, and some dad jokes to be woven into each presentation and conversation.",
-                "position": "2x3",
-                "preset": "ExpandableText",
+                "position": "3x2",
+                "preset": "AboutExtraText",
                 "showLimitedText": true,
                 "maximumCharacters": 0,
                 "isExpandable": true,
