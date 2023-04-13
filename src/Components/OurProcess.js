ad33289566edb112fbac35bfb7be2b05899e39dd
Arsalan31 <87613869+Arsalan31@users.noreply.github.com>
2023-04-13 18:01:55 +0500

    13/04/2023 6:01PM

---
+++ src/Components/OurProcess.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function OurProcess() {
+    return (
+        <div>
+            <h2 className="uppercase text-blue-80 text-4.5xl font-light text-center pt-8 pb-5">Our Process</h2>
+            <div>
+                <p className="text-gray-15 text-lg font-light text-center">Our six-step process ensures our solutions address the challenges and pain points<br /> associated with the intricate dynamics of your organizational systems and culture.</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default OurProcess;
--- src/Riversroad.js
+++ src/Riversroad.js
@@ -6,6 +6,7 @@ import Footer3 from "./Components/Footer3";
 import Header3 from "./Components/Header3";
 import Hero3 from "./Components/Hero3";
 import Solutions from "./Components/Solutions";
+import OurProcess from "./Components/OurProcess";
 
 
 function Riversroads() {
@@ -16,6 +17,7 @@ function Riversroads() {
                 <Header3 />
                 <Hero3 isRtl={isRtl} setIsRtl={setIsRtl} />
                 <Solutions />
+                <OurProcess />
                 <Development />
                 <About />
                 {/* <Contact1 /> */}
